8c3ede204dfae438839e08f3e313eb245857cb2f
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-03-25 19:16:09 -0300

    Adicionado validação de nome

--- perguntarNome.java
+++ perguntarNome.java
@@ -5,11 +5,22 @@ public class perguntarNome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nome;
+        boolean validName = false;
 
         do {
             System.out.println("Qual o seu nome?");
             nome = scanner.nextLine();
-        } while(nome == null || nome.isEmpty());
+
+            if(nome != null && !(nome.trim().isEmpty()) && nome.matches("[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+")) {
+                validName = true;
+                break;
+            }
+
+            System.out.println("-----");
+            System.out.println("Por favor, digite um nome válido.");
+            System.out.println("-----");
+
+        } while(!validName);
 
         System.out.println("Seu nome é " + nome + "!");
     }
